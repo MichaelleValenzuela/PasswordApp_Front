a9d31f2f4422ad3cd15e6ee361605a880881c4eb
Michaelle Valenzuela <michaelle.valenzuela@epn.edu.ec>
2024-02-07 10:50:53 -0500

    tailwindconfig purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     content: [
         './index.html', './src/**/*.{vue,js,ts,jsx,tsx,css}'
     ],
-    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    //purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
         extend: {},
     },
